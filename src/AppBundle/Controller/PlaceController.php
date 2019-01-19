94bd1e070368ae07c2a14475655d11d6ce27ec46
Cédric Oloa <cedric.oloa@laposte.net>
2019-01-19 15:58:49 +0100

    Format automatique

--- src/AppBundle/Controller/PlaceController.php
+++ src/AppBundle/Controller/PlaceController.php
@@ -1,59 +1,45 @@
 <?php
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations as Rest; // alias pour toutes les annotations
 use AppBundle\Entity\Place;
 
 class PlaceController extends Controller
 {
+
     /**
-     * @Route("/places", name="places_list")
-     * @Method({"GET"})
+     * @Rest\View()
+     * @Rest\Get("/places")
      */
     public function getPlacesAction(Request $request)
     {
         $places = $this->get('doctrine.orm.entity_manager')
-                ->getRepository('AppBundle:Place')
-                ->findAll();
+            ->getRepository('AppBundle:Place')
+            ->findAll();
         /* @var $places Place[] */
 
-        $formatted = [];
-        foreach ($places as $place) {
-            $formatted[] = [
-               'id' => $place->getId(),
-               'name' => $place->getName(),
-               'address' => $place->getAddress(),
-            ];
-        }
-
-        return new JsonResponse($formatted);
+        return $places;
     }
 
     /**
-     * @Route("/places/{place_id}", requirements={"place_id" = "\d+"}, name="places_one")
-     * @Method({"GET"})
+     * @Rest\View()
+     * @Rest\Get("/places/{id}")
      */
     public function getPlaceAction(Request $request)
     {
         $place = $this->get('doctrine.orm.entity_manager')
             ->getRepository('AppBundle:Place')
-            ->find($request->get('place_id'));
+            ->find($request->get('id')); // L'identifiant en tant que paramétre n'est plus nécessaire
         /* @var $place Place */
 
         if (empty($place)) {
             return new JsonResponse(['message' => 'Place not found'], Response::HTTP_NOT_FOUND);
         }
 
-        $formatted = [
-            'id' => $place->getId(),
-            'name' => $place->getName(),
-            'address' => $place->getAddress(),
-        ];
-
-        return new JsonResponse($formatted);
+        return $place;
     }
 }
